01c9133d0d8426e7f69537f2ad0337ce835df7e2
oncomouse <oncomouse@gmail.com>
2022-04-14 12:07:57 -0500

    Fix some config.def.h issues

--- config.def.h
+++ config.def.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx = 1;        /* border pixel of windows */
 static const unsigned int snap     = 32;       /* snap pixel */
 static const int showbar           = 1;        /* 0 means no bar */
 static const int topbar            = 1;        /* 0 means bottom bar */
+static const int focusedontop      = 1;
 static const char *fonts[]         = { "monospace:size                       = 10" };
 static const char dmenufont[]      = "monospace:size                         = 10";
 static char normbgcolor[]          = "#222222";
@@ -70,6 +71,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "=[]",      rtile },
 };
 
 /* key definitions */
